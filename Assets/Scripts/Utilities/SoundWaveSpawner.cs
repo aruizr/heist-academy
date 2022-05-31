a113bc5d7269b010fb2abe208ec9041b64ac9b55
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-05-31 20:51:31 +0200

    Sound Wave WIP

---
+++ Assets/Scripts/Utilities/SoundWaveSpawner.cs
@@ -0,0 +1,33 @@
+﻿using Codetox.Pooling;
+using UnityEngine;
+
+namespace Utilities
+{
+    public class SoundWaveSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject prefab;
+
+        private Pool<GameObject> _pool;
+
+        private void Awake()
+        {
+            _pool = new LifoPool<GameObject>(
+                () =>
+                {
+                    var obj = Instantiate(prefab);
+                    obj.SetActive(false);
+                    return obj;
+                },
+                obj =>
+                {
+                    
+                }
+            );
+        }
+
+        public void SpawnWave(GameObject source)
+        {
+            
+        }
+    }
+}
