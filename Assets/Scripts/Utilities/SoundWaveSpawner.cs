87100592d0b127f789562c2b9b978285d361027b
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-06-01 01:55:10 +0200

    Sound waves

--- Assets/Scripts/Utilities/SoundWaveSpawner.cs
+++ Assets/Scripts/Utilities/SoundWaveSpawner.cs
@@ -1,4 +1,8 @@
-﻿using Codetox.Pooling;
+﻿using System.Collections;
+using Codetox.Messaging;
+using Codetox.Pooling;
+using DG.Tweening;
+using Sensors;
 using UnityEngine;
 
 namespace Utilities
@@ -6,6 +10,9 @@ namespace Utilities
     public class SoundWaveSpawner : MonoBehaviour
     {
         [SerializeField] private GameObject prefab;
+        [SerializeField] private int poolSize = 10;
+        [SerializeField] private float expansionTime;
+        [SerializeField] private Ease waveEase;
 
         private Pool<GameObject> _pool;
 
@@ -18,16 +25,35 @@ namespace Utilities
                     obj.SetActive(false);
                     return obj;
                 },
-                obj =>
-                {
-                    
-                }
+                obj => obj.SetActive(true),
+                obj => obj.SetActive(false),
+                Destroy,
+                poolSize
             );
         }
 
         public void SpawnWave(GameObject source)
         {
+            source.Send<SoundEmitter>(emitter =>
+            {
+                var wave = _pool.Get();
+                
+                wave.transform.position = source.transform.position;
+                wave.transform.up = Vector3.up;
+                wave.transform.localScale = Vector3.one * 0.1f;
+                wave.transform.
+                    DOScale(Vector3.one * emitter.Radius * 2, expansionTime).
+                    SetEase(waveEase).
+                    OnComplete(() => _pool.Return(wave));
             
+                wave.Send<SpriteRenderer>(spriteRenderer =>
+                {
+                    var color = spriteRenderer.color;
+
+                    spriteRenderer.color = new Color(color.r, color.g, color.b, 0.5f);
+                    spriteRenderer.DOFade(0, expansionTime).SetEase(waveEase);
+                }, MessageScope.Children);
+            });
         }
     }
 }
