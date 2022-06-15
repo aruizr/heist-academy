deda666d93b8b89ccf8761511ffc702f9e055ff7
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-06-15 03:33:17 +0200

    Pasitos para el jugador y los guardias

---
+++ Assets/Scripts/Audio/AudioClipRandomizer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Audio
+{
+    public class AudioClipRandomizer : MonoBehaviour
+    {
+        [SerializeField] private List<AudioClip> clips;
+
+        public UnityEvent<AudioClip> onPickedRandom;
+
+        private int _lastIndex;
+
+        public void PickRandom()
+        {
+            var index = 0;
+
+            do
+            {
+                index = Random.Range(0, clips.Count);
+            } while (index == _lastIndex);
+
+            var clip = clips[index];
+            _lastIndex = index;
+
+            onPickedRandom?.Invoke(clip);
+        }
+    }
+}
---
+++ Assets/Scripts/Audio/StepAudioClipPicker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+using Random = UnityEngine.Random;
+
+namespace Audio
+{
+    public class StepAudioClipPicker : MonoBehaviour
+    {
+        [SerializeField] private List<StepSet> stepSets;
+        
+        public UnityEvent<AudioClip> onStepPicked;
+
+        private StepSet _currentStepSet;
+        private int _lastIndex;
+
+        [Serializable]
+        public class StepSet
+        {
+            public string surface;
+            public List<AudioClip> clips;
+        }
+
+        public void SetCurrentSurface(GameObject surface)
+        {
+            var surfaceName = surface.tag;
+            _currentStepSet = stepSets.FirstOrDefault(stepSet => stepSet.surface == surfaceName);
+            _lastIndex = 0;
+        }
+
+        public void PickStepClip()
+        {
+            if (_currentStepSet == null) return;
+            
+            var index = 0;
+            var clips = _currentStepSet.clips;
+
+            do
+            {
+                index = Random.Range(0, clips.Count);
+            } while (index == _lastIndex);
+
+            var clip = clips[index];
+            _lastIndex = index;
+
+            onStepPicked?.Invoke(clip);
+        }
+    }
+}
