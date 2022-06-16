6f2c4955757565a9afac4870e18434fc5418b28d
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-06-16 20:31:32 +0200

    Fred c mueve re lindo

---
+++ Assets/Scripts/Utilities/RandomAnimationTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utilities
+{
+    public class RandomAnimationTrigger : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+        [SerializeField] private List<string> triggers;
+        [SerializeField] private Range<float> timeRange;
+
+        private void OnEnable()
+        {
+            StartCoroutine(DoRandomAnimation());
+        }
+
+        private void OnDisable()
+        {
+            StopCoroutine(DoRandomAnimation());
+        }
+
+        private IEnumerator DoRandomAnimation()
+        {
+            while (true)
+            {
+                var time = Random.Range(timeRange.min, timeRange.max);
+                yield return new WaitForSeconds(time);
+                var index = Random.Range(0, triggers.Count);
+                var trigger = triggers[index];
+                animator.SetTrigger(trigger);
+            }
+        }
+    }
+}
